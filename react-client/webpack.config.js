83b83e814387ce1cf8ff883285f48310cf298bc0
spara <spara@github.com>
2017-09-28 14:59:21 -0700

    updated webpack to pass env vars

--- react-client/webpack.config.js
+++ react-client/webpack.config.js
@@ -24,8 +24,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env' : {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'API_HOST': JSON.stringify(process.env.API_HOST || "localhost:8080")
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "production"),
+        API_HOST: JSON.stringify(process.env.API_HOST || "localhost:8080")
       }
     })
   ]
