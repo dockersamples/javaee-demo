a202f09eccdb578df9ee449eef4afe9e0a7746fe
Michael Irwin <mikesir87@gmail.com>
2017-10-04 22:04:32 -0400

    Ensure API_HOST is set correctly when npm run build is executed

--- add_pwd_host.sh
+++ add_pwd_host.sh
@@ -5,4 +5,4 @@ hostname=$(echo $host_ip | sed 's/\./-/g')
 FQDN=${PWD_HOST_FQDN}
 API_ENDPOINT="pwd$hostname-8080.$FQDN"
   
-sed -i "2i ENV API_HOST=${API_ENDPOINT}" ./react-client/Dockerfile
+sed -i -e "s/localhost/${API_ENDPOINT}/" ./react-client/package.json
