e4160fb77c4d2a8907baa0004512279909135bbd
spara <spara@github.com>
2018-03-22 04:56:05 -0400

    updated for deploy on kubernetes

--- add_ee_pwd_host.sh
+++ add_ee_pwd_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PUBLIC_IP=$(ip addr | grep 172.18. | awk '{print $2}' | awk -F / '{print $1}')
-API_ENDPOINT="ip${PUBLIC_IP//./-}-${SESSION_ID}.direct.${PWD_HOST_FQDN}:8080"
+API_ENDPOINT="ip${PUBLIC_IP//./-}-${SESSION_ID}.direct.${PWD_HOST_FQDN}:30080"
 
 sed -i "2i ENV API_HOST=${API_ENDPOINT}" ./react-client/Dockerfile
 
